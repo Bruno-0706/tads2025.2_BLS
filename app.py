23f8d4ab25b4dd94880bd67ea17b3cfe30db02f0
Bruno-0706 <bls17@discente.ifpe.edu.br>
2026-02-04 12:34:48 +0000

    app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from functions.plot import plot_ts
+
+st.title("O nomezinho muito brabo")
+st.write("DAAAAAAAAAAAAAAAAAAAAAAAAMN")
+
+ticker = st.sidebar.text_input("Escolhe aí menó", value="AAPL")
+
+fig = plot_ts(ticker)
+
+st.plotly_chart(fig)
